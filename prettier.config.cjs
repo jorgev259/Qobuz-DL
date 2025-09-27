3517d684917093c0fec7cbc3df05c4febc6b1583
FarmzDev <153562375+FarmzDev@users.noreply.github.com>
2025-09-27 15:06:25 +0100

    Changed Indent Size | Fixed Minor CSS Issues

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const prettierConfigStandard = require("prettier-config-standard");
+const prettierConfigStandard = require('prettier-config-standard');
 
 /** @type {import("prettier").Config} */
-const config = { ...prettierConfigStandard, printWidth: 120 };
+const config = { ...prettierConfigStandard, printWidth: 120, semi: true, tabWidth: 4 };
 
 module.exports = config;
