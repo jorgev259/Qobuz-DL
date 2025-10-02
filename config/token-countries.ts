c24b20b43e687b7bd1589f703f7d230676ebc1c2
Jorge Vargas <jorgev259@outlook.es>
2025-10-01 20:28:18 -0600

    Use country list from environment variable

--- config/token-countries.ts
+++ config/token-countries.ts
@@ -1,4 +1,6 @@
 import { getRandomToken } from '@/lib/qobuz-dl-server';
+import { getRandomItem } from '@/lib/utils';
+import * as z from 'zod';
 
 export type TokenCountry = {
     code: string;
@@ -8,14 +10,25 @@ export type TokenCountry = {
 // Country codes should follow ISO 3166-1 alpha-2 format (https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2)
 // If you fill this list, process.env.QOBUZ_AUTH_TOKENS will be ignored
 
-export const tokenCountriesMap: TokenCountry[] = [];
-// Example usage
-// [
-//     {
-//         code: 'US',
-//         token: 'TOKEN HERE'
-//     }
-// ]
+const TokenSchema = z.record(z.array(z.string()));
+const tokenStoreString = process.env.QOBUZ_AUTH_TOKENS;
 
-export const getTokenForCountry = (country: string): string =>
-    tokenCountriesMap.find((c) => c.code.toUpperCase() === country.toUpperCase())?.token || getRandomToken();
+if (!tokenStoreString) throw new Error('Deployment is missing QOBUZ_AUTH_TOKENS environment variable.');
+export let tokenStore: z.infer<typeof TokenSchema>;
+
+try {
+    tokenStore = JSON.parse(tokenStoreString);
+} catch {
+    throw new Error('Failed to parse QOBUZ_AUTH_TOKENS environment variable.');
+}
+
+const result = TokenSchema.safeParse(tokenStore);
+if (!result.success) throw new Error(result.error.message);
+
+export const countryCodes = Object.keys(tokenStore);
+export const defaultCountry = countryCodes[0];
+
+export function getTokenForCountry(country: string | undefined) {
+    if (!country) return getRandomToken();
+    return getRandomItem(tokenStore[country]) || getRandomToken();
+}
